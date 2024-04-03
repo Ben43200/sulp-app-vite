709615701b0eefd77734f9feab40d95791787cff
meteoben <meteoben@gmail.com>
2024-04-03 07:24:03 +0200

    mise en place home.scss de sulp-app

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
   const homeIntro =
     "Bienvenue sur le site Ekklêsia Web. Nous accompagnons les Eglises locales dans leur croissance en permettant une plus grande visibilité sur Internet. A l’écoute des besoins spécifiques de chaque paroisse, nous proposons des sites Web clés en main, à prix serrés. Nous concevons également des solutions sur mesure et professionnelles, sécurisées et évolutives.";
   return (
-    <main>
+    <main className="home-container">
       <p className="intro">{homeIntro}</p>
       {/* <Card /> */}
       <div className="carousel-container">
