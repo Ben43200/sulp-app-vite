dcb67752a2e247af68882a7124c9ccaa0067ab71
meteoben <meteoben@gmail.com>
2024-05-28 15:12:46 +0200

    nettoyage code home jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -14,24 +14,14 @@ import Contact from "../../components/contact/Contact.jsx";
 
 // import MyMapComponent from "../../components/mymap/MyMap.jsx";
 function Home() {
+  console.log(logo.logo1);
   useEffect(() => {
     const img = new Image();
     img.src = Carte;
 }, []);
-  // const cld = new Cloudinary({cloud: {cloudName: 'dtvojwz5j'}});
-  // const homeIntro =
-  //   "Bienvenue sur le site Ekklêsia Web. Nous accompagnons les Eglises locales dans leur croissance en permettant une plus grande visibilité sur Internet. A l’écoute des besoins spécifiques de chaque paroisse, nous proposons des sites Web clés en main, à prix serrés. Nous concevons également des solutions sur mesure et professionnelles, sécurisées et évolutives.";
+ 
   return (
     <main className="home-container">
-    
-      {/* <MyMapComponent /> */}
-      {/* <p className="intro">{homeIntro}</p> */}
-      {/* <div className="google-map">
-      <Link to="https://maps.app.goo.gl/UBfWP5JLSstGKZf29" target="_blank" rel="noreferrer">
-        <img className="carte" src={Google} alt="carte" />
-        </Link> */}
-      {/* </div> */}
-      {/* <Card /> */} 
       <Link to="https://maps.app.goo.gl/UBfWP5JLSstGKZf29" target="_blank" rel="noopener noreferrer">
       <div className="map-container">
         <img className="carte" src={Carte.carte} alt="carte" />
