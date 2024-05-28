2a9eb33a4f5c87e5d07dc60b1b6874c4dd10be9a
meteoben <meteoben@gmail.com>
2024-05-28 05:58:52 +0200

    ajout woosmap essai push

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,22 +7,26 @@ import  Google from "../../utils/google-map.png"
 
 
 import "./home.scss";
+import WoosmapComponent from "../../components/mymap/WoosmapComponent.jsx";
+// import MyMapComponent from "../../components/mymap/MyMap.jsx";
 function Home() {
   // const cld = new Cloudinary({cloud: {cloudName: 'dtvojwz5j'}});
   // const homeIntro =
   //   "Bienvenue sur le site Ekklêsia Web. Nous accompagnons les Eglises locales dans leur croissance en permettant une plus grande visibilité sur Internet. A l’écoute des besoins spécifiques de chaque paroisse, nous proposons des sites Web clés en main, à prix serrés. Nous concevons également des solutions sur mesure et professionnelles, sécurisées et évolutives.";
   return (
     <main className="home-container">
+      <WoosmapComponent />
+      {/* <MyMapComponent /> */}
       {/* <p className="intro">{homeIntro}</p> */}
-      <div className="google-map">
+      {/* <div className="google-map">
       <Link to="https://maps.app.goo.gl/UBfWP5JLSstGKZf29" target="_blank" rel="noreferrer">
         <img className="carte" src={Google} alt="carte" />
-        </Link>
-      </div>
+        </Link> */}
+      {/* </div> */}
       {/* <Card /> */}
-      <div className="carousel-container">
+      {/* <div className="carousel-container">
         <Carousel />
-      </div>
+      </div> */}
     </main>
   );
 }
